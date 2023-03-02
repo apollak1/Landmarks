ae30a7dfb1436821df20b20e511cfe31964746b6
Andrew Pollak <apollak@cps.edu>
2023-03-02 12:29:18 -0600

    Pushing to GitHub the first time.

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Turtle Rock")
+            Text("Turtle Rock!")
                 .font(.title)
         }
             
