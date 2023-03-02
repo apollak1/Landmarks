77ada7064095a885e654f83376ad1d1ddbc47e86
Andrew Pollak <apollak@cps.edu>
2023-03-02 12:19:32 -0600

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Pollak, Andrew on 3/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Turtle Rock")
+                .font(.title)
+        }
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
